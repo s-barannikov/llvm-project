3df3c72a5b811f1474c3d27ca494e6faa07f9c3e
Sergei Barannikov <barannikov88@gmail.com>
2024-03-25 23:32:10 +0300

    Tmp

--- llvm/utils/TableGen/Common/CodeGenInstAlias.h
+++ llvm/utils/TableGen/Common/CodeGenInstAlias.h
@@ -22,11 +22,9 @@
 
 namespace llvm {
 
-template <typename T> class ArrayRef;
 class CodeGenInstruction;
 class CodeGenTarget;
 class DagInit;
-class SMLoc;
 class Record;
 
 /// CodeGenInstAlias - This represents an InstAlias definition.
@@ -45,19 +43,26 @@ public:
   /// Result).
   CodeGenInstruction *ResultInst;
 
-  struct ResultOperand {
-  private:
+  class ResultOperand {
     std::string Name;
     Record *R = nullptr;
     int64_t Imm = 0;
 
+    ResultOperand(std::string N, Record *R)
+        : Name(std::move(N)), R(R), Kind(K_Record) {}
+    ResultOperand(int64_t Imm) : Imm(Imm), Kind(K_Imm) {}
+    ResultOperand(Record *R) : R(R), Kind(K_Reg) {}
+
   public:
     enum { K_Record, K_Imm, K_Reg } Kind;
 
-    ResultOperand(std::string N, Record *r)
-        : Name(std::move(N)), R(r), Kind(K_Record) {}
-    ResultOperand(int64_t I) : Imm(I), Kind(K_Imm) {}
-    ResultOperand(Record *r) : R(r), Kind(K_Reg) {}
+    static ResultOperand createRecord(std::string N, Record *R) {
+      return ResultOperand(std::move(N), R);
+    }
+    static ResultOperand createImmediate(int64_t Imm) {
+      return ResultOperand(Imm);
+    }
+    static ResultOperand createRegister(Record *R) { return ResultOperand(R); }
 
     bool isRecord() const { return Kind == K_Record; }
     bool isImm() const { return Kind == K_Imm; }
@@ -94,10 +99,6 @@ public:
   std::vector<std::pair<unsigned, int>> ResultInstOperandIndex;
 
   CodeGenInstAlias(Record *R, CodeGenTarget &T);
-
-  bool tryAliasOpMatch(DagInit *Result, unsigned AliasOpNo, Record *InstOpRec,
-                       bool hasSubOps, ArrayRef<SMLoc> Loc, CodeGenTarget &T,
-                       ResultOperand &ResOp);
 };
 
 } // namespace llvm
